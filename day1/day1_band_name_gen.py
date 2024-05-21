b716070cc2c127fa5e828d15cbae136ac7c45f4e
ql <166758235+HamaJjigae@users.noreply.github.com>
2024-05-21 08:51:57 -0600

    first commit, contains day1 project

---
+++ day1/day1_band_name_gen.py
@@ -0,0 +1,8 @@
+print("Welcome to the Band Name Generator")
+
+city = input("What is the name of the city you grew up in?: ")
+pet = input("What is the name of a pet that you own or have owned?: ")
+
+band_name = pet + " " + city
+
+print(f"A possible band name could be: {band_name} ")
